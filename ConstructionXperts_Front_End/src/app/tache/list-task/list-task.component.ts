6d041f765a7373d24813093268ce0eb79eee9c27
oussamaaatifi1 <nisowpro@gmail.com>
2024-09-12 09:14:20 +0100

    j'ai modifier page html list des taches

--- ConstructionXperts_Front_End/src/app/tache/list-task/list-task.component.ts
+++ ConstructionXperts_Front_End/src/app/tache/list-task/list-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Status } from 'src/app/model/Enum/Status';
 import { tache } from 'src/app/model/tache';
 import { TaskServiceService } from 'src/app/service/task-service.service';
 
@@ -11,6 +12,7 @@ import { TaskServiceService } from 'src/app/service/task-service.service';
 export class ListTaskComponent implements OnInit {
 
   taches : tache[]  = [];
+  Status = Status;
 
   constructor(private router : Router , private taskservice :TaskServiceService ){}
   ngOnInit(): void {
@@ -26,4 +28,22 @@ export class ListTaskComponent implements OnInit {
   AddTache(): void {
     this.router.navigate(['/addtache']);
   }
+  editTask(id: number): void {
+    this.router.navigate(['/edit/tache', id]); // Navigate to the edit page
+  }
+
+  deleteTask(id: number): void {
+    if (confirm('Are you sure you want to delete this task?')) {
+      this.taskservice.deleteTache(id).subscribe(
+        () => {
+          this.getAllTaches(); // Refresh the list after deletion
+        },
+        error => {
+          console.error('Error deleting task:', error);
+        }
+      );
+    }
+  }
+
+  
 }
