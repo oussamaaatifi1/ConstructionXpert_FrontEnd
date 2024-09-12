8859a7202fb20cdabfc2672f5a54b4285341a939
oussamaaatifi1 <nisowpro@gmail.com>
2024-09-12 09:13:32 +0100

    j'ai terminer crud supprimer une tache

---
+++ ConstructionXperts_Front_End/src/app/tache/delete-tache/delete-tache.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-tache',
+  templateUrl: './delete-tache.component.html',
+  styleUrls: ['./delete-tache.component.css']
+})
+export class DeleteTacheComponent {
+
+}
