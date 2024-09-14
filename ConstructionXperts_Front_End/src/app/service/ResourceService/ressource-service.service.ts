3509296b9cb07e1fba74360307f70cf11a24831e
simoxxhunter <kingstonegame@gmail.com>
2024-09-14 21:59:27 +0100

    creation service pour resource

--- ConstructionXperts_Front_End/src/app/service/ResourceService/ressource-service.service.ts
+++ ConstructionXperts_Front_End/src/app/service/ResourceService/ressource-service.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {tache} from "../../model/tache";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RessourceServiceService {
 
-  constructor() { }
+
+  constructor(private httpclient : HttpClient) { }
+
+  private BaseUrl = 'http://localhost:8765/api/Ressource';
+
+  getAllResources() : Observable<tache[]>{
+    return this.httpclient.get<tache[]>(`${this.BaseUrl}`);
+  }
+
+  createResources(tacheData: tache): Observable<tache> {
+    return this.httpclient.post<tache>(`${this.BaseUrl}`, tacheData);
+  }
 }
+
