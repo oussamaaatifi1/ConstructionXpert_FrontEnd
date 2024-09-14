dc522d8eb039c2a49c6bbf17cbb038cfcb91c4fa
simoxxhunter <kingstonegame@gmail.com>
2024-09-14 23:01:55 +0100

    ajoute de checmin pour accede a la page d'affichage

--- ConstructionXperts_Front_End/src/app/app-routing.module.ts
+++ ConstructionXperts_Front_End/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListTaskComponent } from './tache/list-task/list-task.component';
 import { AddTaskComponent } from './tache/add-task/add-task.component';
 import {AddresourcesComponent} from "./Resources/addresources/addresources.component";
+import {ShowresourcesComponent} from "./Resources/showresources/showresources.component";
 
 const routes: Routes = [
   {path : 'listtache', component : ListTaskComponent},
   {path : 'addtache',component : AddTaskComponent},
   {path : 'AddResources',component : AddresourcesComponent},
+  {path : 'ShowResources',component : ShowresourcesComponent},
 
 ];
 
